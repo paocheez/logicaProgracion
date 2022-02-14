2f5a522cdd560f5bcfdd47a4f030af9a59902c3c
Paola Conde <pao.cheez@gmail.com>
2022-02-14 14:10:29 -0600

    solution final ex5

--- ejercicio5.js
+++ ejercicio5.js
@@ -1,4 +1,11 @@
 /*
     Sacar el porcentaje de un numero. Mandamos tanto el porcentaje como el numero por parametros.
-Input: (20, 100) -- El 20% de 100 Output: 20
+    Input: (20, 100) -- El 20% de 100 Output: 20
 */
+
+function percent(perc,number){
+    let total;
+    total = (perc*number)/100;
+
+    return `El ${perc}% de ${number} es ${total}`;
+}
